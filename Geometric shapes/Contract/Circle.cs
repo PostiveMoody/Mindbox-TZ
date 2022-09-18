6dee2acce64ffd5d57de5e20fcbb57f58e591a24
PostiveMoody <83549879+PostiveMoody@users.noreply.github.com>
2022-09-18 18:49:33 +0300

    Техническое задание MindBox

---
+++ Geometric shapes/Contract/Circle.cs
@@ -0,0 +1,26 @@
+﻿using Run;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract
+{
+    public class Circle : IShape
+    {
+        private double Radius;
+        public Circle(double radius)
+        {
+            if (radius < 0)
+                throw new ArgumentOutOfRangeException("Радиус круга не может быть меньше нуля");
+
+            this.Radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius,2);
+        }
+    }
+}
