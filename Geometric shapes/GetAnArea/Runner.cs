604594b6c3596739b1d2245b56a7e9a7134465c1
PostiveMoody <83549879+PostiveMoody@users.noreply.github.com>
2022-09-19 08:45:46 +0300

    ТЗ Mindbox C#
    Использовал один интерфейс и класс Runner где используется паттерн стратегия для вычисление площади фигуры без знания типа фигуры в compile-time.

---
+++ Geometric shapes/GetAnArea/Runner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Contract;
+using Run;
+
+public class Runner 
+{
+    static void Main(string[] args)
+    {
+        
+        Triangle triangle = new Triangle(3,4,5);
+
+        Execute(triangle);
+
+        Console.WriteLine("Был создана фигура: " + triangle.FigureName);
+
+        if (triangle.RightTriangle)
+            Console.WriteLine("Треугольник является прямоугольным");
+
+
+        Circle circle = new Circle(5);
+        Console.WriteLine("Был создана фигура: " + circle.FigureName);
+
+        Console.ReadLine();
+    }
+    public static void Execute(IShape shape)
+    {
+        shape.GetArea();
+    }
+}
---
+++ Geometric shapes/Run/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Run
+{
+    public class Execute
+    {
+        public void GetArea()
+        {
+
+        }
+    }
+}
